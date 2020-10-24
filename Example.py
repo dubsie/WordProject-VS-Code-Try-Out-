3dcefc23118f032f29a455851a4189dcbed20dd9
dubsie <dwaterson68@gmail.com>
2020-10-24 22:58:08 +0200

    My next change

--- Example.py
+++ Example.py
@@ -2,7 +2,9 @@ import sys
 
 print (sys.version)
 
-# my_name = input("Your name? ")
-# print ("Hello,",my_name)
+my_name = input("Your name? ")
+print ("Hello,",my_name)
+
+print ("my name is lucia")
 
 # print ('Hello World')
